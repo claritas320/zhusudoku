bd6554a226a80d035baa83a7e10824d972482161
claritas320 <53292169+claritas320@users.noreply.github.com>
2024-01-06 14:40:15 +0000

    Update .gitignore, package-lock.json, package.json, and add new files and modifications

--- pages/sudoku.js
+++ pages/sudoku.js
@@ -6,7 +6,9 @@ import dynamic from 'next/dynamic'
 import styles from '../styles/Sudoku.module.css';
 import { useState, useEffect } from "react";
 
-import { Pool } from 'pg';
+
+const { Pool } = require('pg')
+require('dotenv').config()
 
 const pool = new Pool({
   connectionString: "postgres://default:mp6SkYVC2MGv@ep-lively-leaf-12069118-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
