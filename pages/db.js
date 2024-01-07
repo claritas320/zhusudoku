24e568b94828858ec4e01659df8f81a70089a3a6
claritas320 <53292169+claritas320@users.noreply.github.com>
2024-01-07 05:05:39 +0000

    modified:   pages/db.js

--- pages/db.js
+++ pages/db.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config(); // 加载 .env 文件中的环境变量
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DATABASE,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
 });
 
 export async function saveGridToDatabase(grid) {
